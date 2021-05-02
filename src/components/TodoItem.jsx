7397a00d3daa43113f5de9cb7657d87bddda0efa
Eli <ezsto92@gmail.com>
2021-05-02 19:47:30 -0400

    Mostly finished, list numbers weird

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deleteTodo, updateTodo } from "../redux/actions";
 
-function TodoItem(props) {
+function TodoItem({ todo }) {
+  const [editable, setEditable] = useState(false);
+  const [name, setName] = useState(todo.name);
+  let dispatch = useDispatch();
   return (
     <div>
       <div className="row mx-2 align-items-center">
-        <div>1</div>
+        <div>#{todo.id.length > 1 ? todo.id[2] : todo.id}</div>
         <div className="col">
-          <h4>Todo Title</h4>
+          {editable ? (
+            <input
+              type="text"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          ) : (
+            <h4>{todo.name}</h4>
+          )}
         </div>
-        <button className="btn btn-primary m-2">Edit</button>
-        <button className="btn btn-danger m-2">Delete</button>
+        <button
+          onClick={() => {
+            dispatch(
+              updateTodo({
+                ...todo,
+                name: name,
+              })
+            );
+            if (editable) {
+              setName(todo.name);
+            }
+            setEditable(!editable);
+          }}
+          className="btn btn-primary m-2"
+        >
+          {editable ? "Update" : "Edit"}
+        </button>
+        <button
+          onClick={() => dispatch(deleteTodo(todo.id))}
+          className="btn btn-danger m-2"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
